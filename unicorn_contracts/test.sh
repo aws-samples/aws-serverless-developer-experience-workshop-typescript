fdaa16c212270498199b34a7159b9f561b59bc40
kechn <kechn@amazon.com>
2023-09-10 18:36:30 +1000

    python integration tests

--- unicorn_contracts/test.sh
+++ unicorn_contracts/test.sh
@@ -1,18 +1,28 @@
-ApiUrl="https://kjki27n5gd.execute-api.ap-southeast-2.amazonaws.com/Local/"
+#echo '{
+#    "address": {
+#        "country": "USA",
+#        "city": "Anytown",
+#        "street": "Main Street",
+#        "number": 111
+#    },
+#    "seller_name": "John Doe",
+#    "property_id": "usa/anytown/main-street/'$1'"
+#}'
+#
+#
+#curl --location --request POST "https://bxzn3p9db9.execute-api.ap-southeast-2.amazonaws.com/local/contract" \
+#    --header 'Content-Type: application/json' \
+#    --data-raw '{
+#        "address": {
+#            "country": "USA",
+#            "city": "Anytown",
+#            "street": "Main Street",
+#            "number": 111
+#        },
+#        "seller_name": "John Doe",
+#        "property_id": "usa/anytown/main-street/'$1'"
+#        }'
 
-echo "${ApiUrl}contracts" 
-echo ""
-
-
-curl --location --request POST 'https://kjki27n5gd.execute-api.ap-southeast-2.amazonaws.com/Local/contracts' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "address": {
-        "country": "USA",
-        "city": "Anytown",
-        "street": "Main Street",
-        "number": 111
-    },
-    "seller_name": "John Doe",
-    "property_id": "usa/anytown/main-street/111"
-}'
+curl --location --request PUT "https://bxzn3p9db9.execute-api.ap-southeast-2.amazonaws.com/local/contract" \
+    --header 'Content-Type: application/json' \
+    --data-raw ' { "property_id": "usa/anytown/main-street/123" }'
