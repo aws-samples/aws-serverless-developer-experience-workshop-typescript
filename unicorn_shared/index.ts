d15f8c1586a2cbbb316a73e536c0077f9c0c1a11
kevinwochan <kevinwochan@gmail.com>
2024-02-05 15:55:29 +1100

    refactored namespaces, shared functions and shared types

--- unicorn_shared/index.ts
+++ unicorn_shared/index.ts
@@ -1,54 +1,29 @@
+
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 
-import * as UnicornSharedConstruct from './constructs';
-
-export { UnicornSharedConstruct };
-
-export enum UNICORN_NAMESPACES {
-    CONTRACTS = 'unicorn.contracts',
-    PROPERTIES = 'unicorn.properties',
-    WEB = 'unicorn.web',
-} // should this be direct string
+export const UNICORN_CONTRACTS_NAMESPACE:string = "unicorn.contracts";
+export const UNICORN_PROPERTIES_NAMESPACE:string = "unicorn.properties";
+export const UNICORN_WEB_NAMESPACE:string = "unicorn.wev";
 
 /** The different stages for the app. */
 export enum Stage {
-    local = 'local',
-    dev = 'dev',
-    prod = 'prod',
+    local = "local",
+    dev = "dev",
+    prod = "prod",
 }
-
-// API Contracts
-// Opinionated CDK Constructs
-// Use shared types instead of schemas?
-// Bus to bus, lose control, source chain, pass event id from previous service
-// uidv7/v8 time sortable. propid
 
 export const isProd = (stage: Stage) => stage === Stage.prod;
 
-export const logsRetentionPeriod = (stage: Stage) => {
+export const LogsRetentionPeriod = (stage: Stage) => {
     switch (stage) {
         case Stage.local:
             return RetentionDays.ONE_DAY;
         case Stage.dev:
             return RetentionDays.ONE_WEEK;
         case Stage.prod:
-            return RetentionDays.TWO_WEEKS;
+            return RetentionDays.TWO_WEEKS
         default:
             return RetentionDays.ONE_DAY;
-    }
-};
 
-export const eventBusName = (stage: Stage, namespace: UNICORN_NAMESPACES) => {
-    switch (namespace) {
-        case UNICORN_NAMESPACES.CONTRACTS:
-            return `UnicornContractsBus-${stage}`; // use the
-        case UNICORN_NAMESPACES.PROPERTIES:
-            return `UnicornPropertiesBus-${stage}`;
-        case UNICORN_NAMESPACES.WEB:
-            return `UnicornWebBus-${stage}`;
-        default:
-            throw new Error(
-                `Error generatinig Event Bus Name Unknown namespace: ${namespace}`
-            );
     }
-};
+}
