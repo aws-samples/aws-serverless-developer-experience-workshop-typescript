aff16f6c8735f9e4a0cfad3ae6f36cd9e3e9df22
kevinwochan <kevinwochan@gmail.com>
2024-02-05 15:38:58 +1100

    shared workspace namespaces and CDK native RestApi

---
+++ shared/index.ts
@@ -0,0 +1,3 @@
+export const UNICORN_CONTRACTS_NAMESPACE:string = "unicorn.contracts";
+export const UNICORN_PROPERTIES_NAMESPACE:string = "unicorn.properties";
+export const UNICORN_WEB_NAMESPACE:string = "unicorn.wev";
