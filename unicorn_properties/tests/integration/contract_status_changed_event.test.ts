2c95db2d3539abf3c93993452f6da606030b1e99
kevinwochan <kevinwochan@gmail.com>
2024-02-11 01:58:21 +1100

    prettier formatting tests

--- unicorn_properties/tests/integration/contract_status_changed_event.test.ts
+++ unicorn_properties/tests/integration/contract_status_changed_event.test.ts
@@ -1,16 +1,31 @@
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
-import { sleep, findOutputValue, sendContractStatusChanged } from "./helper";
+import {
+  sleep,
+  findOutputValue,
+  sendContractStatusChanged,
+  clearDatabase,
+} from "./helper";
 
 describe("Testing draft contract event handling", () => {
+  beforeAll(async () => {
+    // Clear DB
+    await clearDatabase();
+  });
+
+  afterAll(async () => {
+    // Clear DB
+    await clearDatabase();
+  });
+
   it("Should create a draft contract", async () => {
     const ddb = new DynamoDBClient({});
     const contractStatusTableName = await findOutputValue(
-      "ContractStatusTableName",
+      "ContractStatusTableName"
     );
     await sendContractStatusChanged(
       "usa/anytown/main-street/111",
       "f2bedc80-3dc8-4544-9140-9b606d71a6ee",
-      "DRAFT",
+      "DRAFT"
     );
 
     await sleep(5000);
@@ -21,9 +36,9 @@ describe("Testing draft contract event handling", () => {
         Key: {
           property_id: { S: "usa/anytown/main-street/111" },
         },
-      }),
+      })
     );
-    console.log(ddbResp);
+
     expect(ddbResp.Item).toBeTruthy();
     if (!ddbResp.Item) throw Error("Contract not found");
     expect(ddbResp.Item.contract_status?.S).toBe("DRAFT");
@@ -33,12 +48,12 @@ describe("Testing draft contract event handling", () => {
   it("Should update an existing contract status to APPROVED", async () => {
     const ddb = new DynamoDBClient({});
     const contractStatusTableName = await findOutputValue(
-      "ContractStatusTableName",
+      "ContractStatusTableName"
     );
     await sendContractStatusChanged(
       "usa/anytown/main-street/111",
       "f2bedc80-3dc8-4544-9140-9b606d71a6ee",
-      "APPROVED",
+      "APPROVED"
     );
 
     await sleep(2000); // Sleep for 2 seconds
@@ -49,7 +64,7 @@ describe("Testing draft contract event handling", () => {
         Key: {
           property_id: { S: "usa/anytown/main-street/111" },
         },
-      }),
+      })
     );
 
     if (!ddbResp.Item) throw new Error("Contract not found");
