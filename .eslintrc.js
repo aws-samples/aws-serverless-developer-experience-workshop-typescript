795a39edfc46ff3f883e493ad350b2eb6ccfac79
kevinwochan <kevinwochan@gmail.com>
2024-02-20 13:59:32 +1100

    linting fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,14 @@ module.exports = {
     sourceType: "module",
   },
   extends: [
+    "prettier",
     "plugin:@typescript-eslint/recommended", // recommended rules from the @typescript-eslint/eslint-plugin
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    "prettier/prettier": "error",
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
+  plugins: ["prettier"],
 };
