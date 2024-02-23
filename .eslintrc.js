799f03b87ac3fdc42086a024e8e7e2aa66074ada
kevinwochan <kevinwochan@gmail.com>
2024-02-23 16:13:03 +1100

    unicorn_contracts code styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,34 @@
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module",
-  },
-  extends: [
-    "prettier",
-    "plugin:@typescript-eslint/recommended", // recommended rules from the @typescript-eslint/eslint-plugin
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
-  rules: {
-    "prettier/prettier": "error",
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-  },
-  plugins: ["prettier"],
-};
+modules.exports = {
+    root: true,
+    ignorePatterns: [
+        "**/*"
+    ],
+    overrides: [
+        {
+            "files": [
+                "*.ts",
+                "*.tsx",
+                "*.js",
+                "*.jsx"
+            ],
+            "rules": {}
+        },
+        {
+            "files": [
+                "*.spec.ts",
+                "*.spec.tsx",
+                "*.spec.js",
+                "*.spec.jsx"
+            ],
+            "env": {
+                "jest": true
+            },
+            "rules": {}
+        },
+        {
+            "files": "*.json",
+            "parser": "jsonc-eslint-parser",
+            "rules": {}
+        }
+    ]
+}
--- unicorn_shared/index.ts
+++ unicorn_shared/index.ts
@@ -1,47 +1,54 @@
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
-import * as UnicornConstructs from './Constructs';
 
-export { UnicornConstructs };
+import * as UnicornSharedConstruct from './constructs';
+
+export { UnicornSharedConstruct };
 
 export enum UNICORN_NAMESPACES {
-  CONTRACTS = 'unicorn.contracts',
-  PROPERTIES = 'unicorn.properties',
-  WEB = 'unicorn.web',
-}
+    CONTRACTS = 'unicorn.contracts',
+    PROPERTIES = 'unicorn.properties',
+    WEB = 'unicorn.web',
+} // should this be direct string
 
 /** The different stages for the app. */
 export enum Stage {
-  local = 'local',
-  dev = 'dev',
-  prod = 'prod',
+    local = 'local',
+    dev = 'dev',
+    prod = 'prod',
 }
+
+// API Contracts
+// Opinionated CDK Constructs
+// Use shared types instead of schemas?
+// Bus to bus, lose control, source chain, pass event id from previous service
+// uidv7/v8 time sortable. propid
 
 export const isProd = (stage: Stage) => stage === Stage.prod;
 
 export const logsRetentionPeriod = (stage: Stage) => {
-  switch (stage) {
-    case Stage.local:
-      return RetentionDays.ONE_DAY;
-    case Stage.dev:
-      return RetentionDays.ONE_WEEK;
-    case Stage.prod:
-      return RetentionDays.TWO_WEEKS;
-    default:
-      return RetentionDays.ONE_DAY;
-  }
+    switch (stage) {
+        case Stage.local:
+            return RetentionDays.ONE_DAY;
+        case Stage.dev:
+            return RetentionDays.ONE_WEEK;
+        case Stage.prod:
+            return RetentionDays.TWO_WEEKS;
+        default:
+            return RetentionDays.ONE_DAY;
+    }
 };
 
 export const eventBusName = (stage: Stage, namespace: UNICORN_NAMESPACES) => {
-  switch (namespace) {
-    case UNICORN_NAMESPACES.CONTRACTS:
-      return `UnicornContractsBus-${stage}`;
-    case UNICORN_NAMESPACES.PROPERTIES:
-      return `UnicornPropertiesBus-${stage}`;
-    case UNICORN_NAMESPACES.WEB:
-      return `UnicornWebBus-${stage}`;
-    default:
-      throw new Error(
-        `Error generatinig Event Bus Name Unknown namespace: ${namespace}`
-      );
-  }
+    switch (namespace) {
+        case UNICORN_NAMESPACES.CONTRACTS:
+            return `UnicornContractsBus-${stage}`; // use the
+        case UNICORN_NAMESPACES.PROPERTIES:
+            return `UnicornPropertiesBus-${stage}`;
+        case UNICORN_NAMESPACES.WEB:
+            return `UnicornWebBus-${stage}`;
+        default:
+            throw new Error(
+                `Error generatinig Event Bus Name Unknown namespace: ${namespace}`
+            );
+    }
 };
