f38f25d6b1ac20d65a9c20b526a0c27d2a7561d6
Stephen Liedig <sliedig@amazon.com>
2025-08-13 14:47:11 +0800

    Unicorn approvals (#214)
    * feat: add command to list the parameters in parameters store
    
    * refactor: renamed UnicornPropertiesNamespace to UnicornApprovalsNamespace in shared namespace definition
    
    * refactor: updated resolve:ssm references for  UnicornPropertiesNamespace to UnicornApprovalsNamespace across multiple templates
    
    * refactor: renamed unicorn_properties with unicorn_approvals
    
    * refactor: update references from unicorn_properties to unicorn_approvals in configuration and code files
    
    * feat: implement contract status management functions in approvals_service, including event handler, approval sync, and status checker
    
    * chore: update Approvals readme
    
    * chore: updated contracts readme
    
    * chore: update web readme
    
    * chore: updated main readme
    
    * chore: update GitHub Actions workflows to use actions/checkout@v4 and actions/github-script@v7
    
    * chore: fix formatting in README.md by adjusting image placement
    
    * chore: upgrade CodeQL action versions to v3 in GitHub workflows
    
    * chore: update architecture diagram
    
    * refactor: update event source and rule names to use 'unicorn.approvals'
    
    * refactor: renames ApprovalService to PublicationManagerService for handling publication approval requests and events
    
    * chore: update Makefiles to reference samconfig.toml instead of samconfig.yaml for stack name
    
    * fix: added missing schema back in
    
    * chore: minor update to readme file
    
    * fix: updating stack outputs for tests to new approvals service
    
    * bug: typos in README.md
    
    * fix: corrects Makefiles
    
    * fix: updates test events to use Approvals eventbus name
    
    * chore: updated approvals readme
    
    * chore: updated readme and spelling mistakes
    
    * fix: syncing templates
    
    * fix: fixes environment variable name for DynamoDB Table
    
    ---------
    
    Co-authored-by: adrianjhunter <4285633+adrianjhunter@users.noreply.github.com>

---
+++ unicorn_approvals/src/approvals_service/contractStatusChangedEventHandler.ts
@@ -0,0 +1,104 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+import { EventBridgeEvent, Context } from 'aws-lambda';
+import type { LambdaInterface } from '@aws-lambda-powertools/commons/types';
+import { MetricUnit } from '@aws-lambda-powertools/metrics';
+import { logger, metrics, tracer } from './powertools';
+import {
+  DynamoDBClient,
+  UpdateItemCommand,
+  UpdateItemCommandInput,
+  UpdateItemCommandOutput,
+} from '@aws-sdk/client-dynamodb';
+import { ContractStatusChanged } from '../schema/unicorn_contracts/contractstatuschanged/ContractStatusChanged';
+import { Marshaller } from '../schema/unicorn_contracts/contractstatuschanged/marshaller/Marshaller';
+
+// Empty configuration for DynamoDB
+const ddbClient = new DynamoDBClient({});
+const DDB_TABLE = process.env.CONTRACT_STATUS_TABLE ?? 'ContractStatusTable';
+
+export interface ContractStatusError extends Error {
+  contract_id: string;
+  name: string;
+  object: any;
+}
+
+class ContractStatusChangedFunction implements LambdaInterface {
+  /**
+   * Handle the contract status changed event from the EventBridge instance.
+   * @param {Object} event - EventBridge Event Input Format
+   * @returns {void}
+   *
+   */
+  @tracer.captureLambdaHandler()
+  @metrics.logMetrics({ captureColdStartMetric: true })
+  @logger.injectLambdaContext({ logEvent: true })
+  public async handler(
+    event: EventBridgeEvent<string, ContractStatusChanged>,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    context: Context
+  ): Promise<void> {
+    logger.info(`Contract status changed: ${JSON.stringify(event.detail)}`);
+    try {
+      // Construct the entry to insert into database.
+      const statusEntry: ContractStatusChanged = Marshaller.unmarshal(
+        event.detail,
+        'ContractStatusChanged'
+      );
+      tracer.putAnnotation('ContractStatus', statusEntry.contractStatus);
+      logger.info(`Unmarshalled entry: ${JSON.stringify(statusEntry)}`);
+
+      // Call saveContractStatus with the entry
+      await this.saveContractStatus(statusEntry);
+    } catch (error: any) {
+      tracer.addErrorAsMetadata(error as Error);
+      logger.error(`Error during DDB UPDATE: ${JSON.stringify(error)}`);
+    }
+    metrics.addMetric('ContractStatusChanged', MetricUnit.Count, 1);
+  }
+
+  /**
+   * Update the ContractStatus entry in the database
+   * @param statusEntry
+   */
+  @tracer.captureMethod()
+  private async saveContractStatus(statusEntry: ContractStatusChanged) {
+    logger.info(
+      `Updating status: ${statusEntry.contractStatus} for ${statusEntry.propertyId}`
+    );
+    const ddbUpdateCommandInput: UpdateItemCommandInput = {
+      TableName: DDB_TABLE,
+      Key: { property_id: { S: statusEntry.propertyId } },
+      UpdateExpression:
+        'set contract_id = :c, contract_status = :t, contract_last_modified_on = :m',
+      ExpressionAttributeValues: {
+        ':c': { S: statusEntry.contractId as string },
+        ':t': { S: statusEntry.contractStatus as string },
+        ':m': { S: statusEntry.contractLastModifiedOn },
+      },
+    };
+    logger.info(`Constructed command ${JSON.stringify(ddbUpdateCommandInput)}`);
+    const ddbUpdateCommand = new UpdateItemCommand(ddbUpdateCommandInput);
+
+    // Send the command
+    const ddbUpdateCommandOutput: UpdateItemCommandOutput =
+      await ddbClient.send(ddbUpdateCommand);
+    logger.info(
+      `Updated status: ${statusEntry.contractStatus} for ${statusEntry.propertyId}`
+    );
+    if (ddbUpdateCommandOutput.$metadata.httpStatusCode != 200) {
+      const error: ContractStatusError = {
+        contract_id: statusEntry.contractId,
+        name: 'ContractStatusDBUpdateError',
+        message:
+          'Response error code: ' +
+          ddbUpdateCommandOutput.$metadata.httpStatusCode,
+        object: ddbUpdateCommandOutput.$metadata,
+      };
+      throw error;
+    }
+  }
+}
+
+export const myFunction = new ContractStatusChangedFunction();
+export const lambdaHandler = myFunction.handler.bind(myFunction);
